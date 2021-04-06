58c11180d571470c0e90cbd909d8faaa6f503981
Stijn Verdenius <stijnverdenius@pop-os.localdomain>
2021-04-06 14:49:18 +0200

    another

--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
-import codecs
-import os.path
-
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("src/example_pkg/version.py", "r", encoding="utf-8") as fh:
+with open("version.py", "r", encoding="utf-8") as fh:
     version = fh.read().split(" = ")[-1]
-
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-
 
 setuptools.setup(
     name="example-banana",  # Replace with your own username
